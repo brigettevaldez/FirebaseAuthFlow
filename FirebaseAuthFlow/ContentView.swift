a3bc63d9b74b66d159fed8cc686108abafd7d5c6
Brigette Valdez <valdez.brigette@gmail.com>
2021-03-18 13:10:05 -0500

    Initial Commit

---
+++ FirebaseAuthFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseAuthFlow
+//
+//  Created by Brigette Valdez on 3/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseAuthFlow/FirebaseAuthFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthFlowApp.swift
+//  FirebaseAuthFlow
+//
+//  Created by Brigette Valdez on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
